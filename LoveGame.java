374ebc25cf217d4472013b037c69933103b12db4
meganmcqueen <megan.mcqueen@yale.edu>
2017-04-19 16:51:13 -0400

    Create LoveGame.java

---
+++ LoveGame.java
@@ -0,0 +1,22 @@
+//************************************************************************
+// File: LoveGame.java         Final Project
+// 
+// Author: <your name>             Email: <your email>
+//
+// Class: LoveGame
+// Dependencies:  
+//
+// Description  :  <insert>
+//  
+//
+//  
+//  
+//
+//************************************************************************
+
+public class LoveGame {
+
+  public static void main(String[] args) {
+  }
+  
+}
